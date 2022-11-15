ffd3afe1da30e9e43b624db8ee14b3d57357272c
Santiago Montaner <santiagomontanergarcia@gmail.com>
2022-11-15 23:41:26 +0100

    Fix tests

--- src/tests/test_commands.py
+++ src/tests/test_commands.py
@@ -27,48 +27,42 @@ class WeatherApiConnectorMock(WeatherApiConnector):
         dates = [self._date + timedelta(days=i) for i in range(days)]
         temperature = Temperature(self._default_temperature_value, units)
         return [WeatherInfo(date, location, temperature, self._weather_description) for date in dates]
-
-class BaseWeatherTestCase:
-
-    class BaseWeatherTest(TestCase):
-
-        command_class = None
-
-        def test_given_correct_input_when_validating_data_then_data_is_valid(self):
-            location = Location("Bilbao", "ES")
-            units = Units.METRIC
-            validation_error_message, validated_input = self.command_class.validate_arguments("Bilbao,ES", "metric")
-            self.assertEqual(len(validation_error_message), 0)
-            self.assertEqual(validated_input[self.command_class.LOCATION], location)
-            self.assertEqual(validated_input[self.command_class.UNITS], units)
-
-        def test_given_location_input_with_spaces_when_validating_data_then_one_validation_message_returned(self):
-            validation_error_message, _ = self.command_class.validate_arguments("Bilbao, ES", "metric")
-            self.assertEqual(len(validation_error_message), 1)
-
-        def test_given_location_input_with_multiple_commas_when_validating_data_then_one_validation_message_returned(self):
-            validation_error_message, _ = self.command_class.validate_arguments("Bilbao,ES,", "metric")
-            self.assertEqual(len(validation_error_message), 1)
-
-        def test_given_incorrect_units_input_when_validating_data_then_then_one_validation_message_returned(self):
-            validation_error_message, _ = self.command_class.validate_arguments("Bilbao,ES", "metricimperial")
-            self.assertEqual(len(validation_error_message), 1)
         
-class CurrentWeatherCommandTestCase(BaseWeatherTestCase.BaseWeatherTest):
+class CurrentWeatherCommandTestCase(TestCase):
 
     def setUp(self):
         initial_date = date(2022, 1, 1)
         self._connector = WeatherApiConnectorMock(initial_date, "Sunny", 5)
         self.command_class = PrintCurrentWeatherCommand
-            
+        
+    def test_given_correct_input_when_validating_data_then_data_is_valid(self):
+        location = Location("Bilbao", "ES")
+        units = Units.METRIC
+        validation_error_message, validated_input = self.command_class.validate_arguments("Bilbao,ES", "metric")
+        self.assertEqual(len(validation_error_message), 0)
+        self.assertEqual(validated_input[self.command_class.LOCATION], location)
+        self.assertEqual(validated_input[self.command_class.UNITS], units)
+
+    def test_given_location_input_with_spaces_when_validating_data_then_one_validation_message_returned(self):
+        validation_error_message, _ = self.command_class.validate_arguments("Bilbao, ES", "metric")
+        self.assertEqual(len(validation_error_message), 1)
+
+    def test_given_location_input_with_multiple_commas_when_validating_data_then_one_validation_message_returned(self):
+        validation_error_message, _ = self.command_class.validate_arguments("Bilbao,ES,", "metric")
+        self.assertEqual(len(validation_error_message), 1)
+
+    def test_given_incorrect_units_input_when_validating_data_then_then_one_validation_message_returned(self):
+        validation_error_message, _ = self.command_class.validate_arguments("Bilbao,ES", "metricimperial")
+        self.assertEqual(len(validation_error_message), 1)
+
     def test_given_correct_input_when_command_is_executed_then_result_is_success(self):
         location = Location("Bilbao", "ES")
         units = Units.METRIC
         command = PrintCurrentWeatherCommand(self._connector, location, units)
         result= command.execute()
-        self.assertEqual(result, CommandResultStatus.Success)
+        self.assertEqual(result, CommandResultStatus.SUCCESS)
     
-class WeatherForecastCommandTestCase(BaseWeatherTestCase.BaseWeatherTest):
+class WeatherForecastCommandTestCase(TestCase):
 
     def setUp(self):
         initial_date = date(2022, 1, 1)
@@ -98,7 +92,7 @@ class WeatherForecastCommandTestCase(BaseWeatherTestCase.BaseWeatherTest):
         units = Units.METRIC
         command = PrintWeatherForecastCommand(self._connector, location, units, days=3)
         result= command.execute()
-        self.assertEqual(result, CommandResultStatus.Success)
+        self.assertEqual(result, CommandResultStatus.SUCCESS)
 
 if __name__ == "__main__":
     main()
